9e40ac7479ae953ed7fcb393de36bb0e87568df7
zhannaa5 <121726316+zhannaa5@users.noreply.github.com>
2023-01-13 23:12:56 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ public class Main {
         persons.add(new Student("John", "Smith", 2.36 ));
         persons.add(new Employee("Michael", "Clark", "doctor", 478000.0));
         persons.add(new Student("Paul", "Flory", 3.57));
-        persons.add(new Employee("Kerem", "Bursin", "enginneer", 450000.0));
+        persons.add(new Employee("Kerem", "Bursin", "engineer", 450000.0));
 
         Collections.sort(persons);
 
